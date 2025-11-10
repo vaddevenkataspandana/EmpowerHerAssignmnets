d9a99c732278f127bf3ff61e3b750c5429033651
vaddevenkataspandana <vaddevenkataspandana@gmail.com>
2025-11-10 17:46:18 +0530

    initial setup done

--- Module_2/Day_5/index.js
+++ Module_2/Day_5/index.js
@@ -1,1 +1,12 @@
-console.log(hello)
+function isPrime(num) {
+  if (num <= 1) return false;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(7));
